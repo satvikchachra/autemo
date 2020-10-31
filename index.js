699859c5c2b4471f752e7b12b052d018647387a2
satvikchachra <satvikchachra19@gmail.com>
2020-10-31 16:58:34 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ app.use('/levelOne', require('./routes/level1'));
 app.use('/levelTwo', require('./routes/level2'));
 app.use('/levelThree', require('./routes/level3'));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
 
 const port = process.env.PORT || 5000;
 
